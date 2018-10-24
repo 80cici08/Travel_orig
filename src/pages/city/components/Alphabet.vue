0185b33e8c99487f17cf5da07c9b9ea3ccd68ee2
薛江阳 <xuejiangyang@xuejiangyangs-MacBook-Air.local>
2018-10-24 16:15:00 -0700

    build

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -5,7 +5,7 @@
       v-for="(item, key) in cities"
       :key="key"
       :ref="key"
-      @touchstart="handleTouchStart"
+      @touchstart.prevent="handleTouchStart"
       @touchmove="handleTouchMove"
       @touchend="handleTouchEnd"
       @click="handleLetterChange"
---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="item" v-for="(item,index) in list" :key="index">
+      <div class="item-title border-bottom">
+        <span class="item-title-icon"></span>
+        {{ item.title }}
+      </div>
+      <detail-list class="item-children" :list="item.children"></detail-list>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .item-title
+    line-height: .8rem
+    font-size: .32rem
+    padding: 0 .2rem
+    .item-title-icon
+      display: inline-block
+      position: relative
+      width: .36rem
+      height: .36rem
+      top: .06rem
+      left: .06rem
+      background: url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat;
+      margin-right: .1rem
+      background-size: .4rem 3rem
+  .item-children
+    padding: 0 .2rem
+</style>
